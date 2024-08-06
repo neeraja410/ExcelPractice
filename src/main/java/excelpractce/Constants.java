a29926539be8560018225bcb1df40074d4e70b40
neeraja410 <mudiredddy95@gmail.com>
2024-08-06 12:48:40 -0700

    Added comments on constants program

--- src/main/java/excelpractce/Constants.java
+++ src/main/java/excelpractce/Constants.java
@@ -1,12 +1,13 @@
 package excelpractce;
 
 public class Constants {
+        //Added my first comment on Constants program
 	static String FIRST_STUDENT_PROGRAM_FILE_PATH = "/Users/jagadesm/eclipse-workspace/First_Student_Program.xlsx";
 	static String SECOND_PROGRAM_EXCEL_FILE_PATH = "/Users/jagadesm/eclipse-workspace/Second_Student_Program.xlsx";
 	static String FIRST_STUDENT_PROGRAM_TABLE = "First_Student_Program";
 	static String Second_Student_Program_table = "Second_Student_Program";
 	static String JDBC_URL="jdbc:mysql://localhost:3306/testdb?useSSL=false";
-    static String JDBC_USER="root";
+        static String JDBC_USER="root";
 	static String JDBC_PASSWORD="neeraJAgadesh1!";
 	static int startingIndex = 1;
 }
